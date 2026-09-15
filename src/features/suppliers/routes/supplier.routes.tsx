282315b5840d12a7391b5ac5a09f21a95e5c0a55
kim280-trustee <pkimani280@gmail.com>
2026-09-15 14:47:59 +0700

    add supplier credit application route

--- src/features/suppliers/routes/supplier.routes.tsx
+++ src/features/suppliers/routes/supplier.routes.tsx
@@ -3,6 +3,7 @@ import { CreateSupplierPage } from "../pages/CreateSupplierPage";
 import { EditSupplierPage } from "../pages/EditSupplierPage";
 import { SupplierPaymentsPage } from "../pages/SupplierPaymentsPage";
 import { SupplierLedgerPage } from "../pages/SupplierLedgerPage";
+import { ApplySupplierCreditPage } from "../pages/ApplySupplierCreditPage";
 
 export const supplierRoutes = [
   { path: "/suppliers", element: <SuppliersPage /> },
@@ -10,4 +11,5 @@ export const supplierRoutes = [
   { path: "/suppliers/:id/edit", element: <EditSupplierPage /> },
   { path: "/suppliers/payments", element: <SupplierPaymentsPage /> },
   { path: "/suppliers/:id/ledger", element: <SupplierLedgerPage /> },
+  { path: "/suppliers/:id/apply-credit", element: <ApplySupplierCreditPage /> },
 ];
