90d49955359c177a7beb302c647f70802626f991
kim280-trustee <pkimani280@gmail.com>
2026-09-15 13:01:36 +0700

    feat: add supplier ledger route

--- src/features/suppliers/routes/supplier.routes.tsx
+++ src/features/suppliers/routes/supplier.routes.tsx
@@ -2,22 +2,12 @@ import { SuppliersPage } from "../pages/SuppliersPage";
 import { CreateSupplierPage } from "../pages/CreateSupplierPage";
 import { EditSupplierPage } from "../pages/EditSupplierPage";
 import { SupplierPaymentsPage } from "../pages/SupplierPaymentsPage";
+import { SupplierLedgerPage } from "../pages/SupplierLedgerPage";
 
 export const supplierRoutes = [
-  {
-    path: "/suppliers",
-    element: <SuppliersPage />,
-  },
-  {
-    path: "/suppliers/create",
-    element: <CreateSupplierPage />,
-  },
-  {
-    path: "/suppliers/:id/edit",
-    element: <EditSupplierPage />,
-  },
-  {
-    path: "/suppliers/payments",
-    element: <SupplierPaymentsPage />,
-  },
+  { path: "/suppliers", element: <SuppliersPage /> },
+  { path: "/suppliers/create", element: <CreateSupplierPage /> },
+  { path: "/suppliers/:id/edit", element: <EditSupplierPage /> },
+  { path: "/suppliers/payments", element: <SupplierPaymentsPage /> },
+  { path: "/suppliers/:id/ledger", element: <SupplierLedgerPage /> },
 ];
