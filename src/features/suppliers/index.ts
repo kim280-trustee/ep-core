2880122ec71ff5be389a5a823e7c0ffd6cf5830b
kim280-trustee <pkimani280@gmail.com>
2026-09-15 12:47:54 +0700

    feat: export supplier credit ledger

--- src/features/suppliers/index.ts
+++ src/features/suppliers/index.ts
@@ -6,47 +6,26 @@
  * ============================================================
  */
 
-
-// Services
-
 export {
   supplierService,
 } from "./services/supplier.service";
-
-
-
-// Hooks
 
 export {
   useSuppliers,
 } from "./hooks/useSuppliers";
 
-
-
-// Store
-
 export {
   useSuppliersStore,
 } from "./store/supplier.store";
 
-
-
-// Types
-
 export type {
-
   Supplier,
-
   CreateSupplierDto,
-
   UpdateSupplierDto,
-
 } from "./types/supplier.types";
-
-
-
-// Routes
 
 export {
   supplierRoutes,
 } from "./routes/supplier.routes";
+
+export * from "./credit-ledger";
