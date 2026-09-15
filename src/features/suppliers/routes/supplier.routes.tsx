d0a1b7beb2cc0af4c08cdbb0ec17054ef8a63f2c
kim280-trustee <pkimani280@gmail.com>
2026-09-15 13:00:07 +0700

    feat: add supplier payments route

--- src/features/suppliers/routes/supplier.routes.tsx
+++ src/features/suppliers/routes/supplier.routes.tsx
@@ -1,51 +1,23 @@
-import {
-  SuppliersPage,
-} from "../pages/SuppliersPage";
-
-
-import {
-  CreateSupplierPage,
-} from "../pages/CreateSupplierPage";
-
-
-import {
-  EditSupplierPage,
-} from "../pages/EditSupplierPage";
-
-
+import { SuppliersPage } from "../pages/SuppliersPage";
+import { CreateSupplierPage } from "../pages/CreateSupplierPage";
+import { EditSupplierPage } from "../pages/EditSupplierPage";
+import { SupplierPaymentsPage } from "../pages/SupplierPaymentsPage";
 
 export const supplierRoutes = [
-
   {
-
-    path:
-      "/suppliers",
-
-    element:
-      <SuppliersPage />,
-
+    path: "/suppliers",
+    element: <SuppliersPage />,
   },
-
-
   {
-
-    path:
-      "/suppliers/create",
-
-    element:
-      <CreateSupplierPage />,
-
+    path: "/suppliers/create",
+    element: <CreateSupplierPage />,
   },
-
-
   {
-
-    path:
-      "/suppliers/:id/edit",
-
-    element:
-      <EditSupplierPage />,
-
+    path: "/suppliers/:id/edit",
+    element: <EditSupplierPage />,
   },
-
+  {
+    path: "/suppliers/payments",
+    element: <SupplierPaymentsPage />,
+  },
 ];
