aacb703980ed9ef48587c46268a095f0e031aa7f
kim280-trustee <pkimani280@gmail.com>
2026-09-18 22:25:35 +0700

    feat(core): expose Supabase organization repositories

--- src/core/organization/repositories/index.ts
+++ src/core/organization/repositories/index.ts
@@ -2,3 +2,5 @@ export * from "./organization.repository";
 
 export * from "./in-memory.organization.repository";
 export * from "./organization-membership.repository";
+export * from "./supabase.organization.repository";
+export * from "./supabase.organization-membership.repository";
